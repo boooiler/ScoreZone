20feca0789e9282a7ba1696d51331d1055e2d4f1
boooiler <sebastianowczarek8@gmail.com>
2024-01-12 19:15:58 +0100

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ScoreZoneApp from './App.tsx'
+import ScoreZoneApp from './root/App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
