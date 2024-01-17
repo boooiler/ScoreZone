89e3aaade129cd79946dd2eceef8e2f58d784a38
boooiler <sebastianowczarek8@gmail.com>
2024-01-17 15:07:41 +0100

    set default homepage

--- src/root/Routes.tsx
+++ src/root/Routes.tsx
@@ -1,21 +1,20 @@
 import React, { Suspense } from "react"
-import { RouteObject } from "react-router-dom"
+import { Navigate, RouteObject } from "react-router-dom"
 import ErrorPage from "./ErrorPage"
-
 import FootballRoutes from '@/views/football/Routes'
 
 const AppWrapper = React.lazy(() => import('./App'))
 
 const routes: RouteObject[] = [
-  // {
-  //   path: '/',
-  //   element: <div>Hello!</div>,
-  //   errorElement: <ErrorPage />
-  // },
+  {
+    path: '/',
+    element: <Navigate to='football' />,
+    errorElement: <ErrorPage />
+  },
   {
     path: '/',
     element: (
-      <Suspense fallback={<div>loading...</div>}>
+      <Suspense>
         <AppWrapper />
       </Suspense>
     ),
