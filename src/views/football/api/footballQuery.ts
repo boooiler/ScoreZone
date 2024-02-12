0aecce39f7ee45fe425c40f364b7f5e045c59127
boooiler <sebastianowczarek8@gmail.com>
2024-02-12 12:33:03 +0100

    fix vercel env 2

--- src/views/football/api/footballQuery.ts
+++ src/views/football/api/footballQuery.ts
@@ -8,7 +8,26 @@ export function useFootballTeams () {
   return useQuery({
     queryKey: ['football_teams'],
     queryFn: async () => {
-      const { data } = await axios.get(`/football-api/teams`, {
+      const { data } = await axios.get(`https://v3.football.api-sports.io/teams`, {
+        headers: {
+          "x-rapidapi-key": apiKey,
+          "x-rapidapi-host": apiHost
+        },
+        params: {
+          season: 2023,
+          league: 107
+        }
+      })
+      return data
+    }
+  })
+}
+
+export function useFootballLeagues () {
+  return useQuery({
+    queryKey: ['football_leagues'],
+    queryFn: async () => {
+      const { data } = await axios.get(`https://v3.football.api-sports.io/leagues`, {
         headers: {
           "x-rapidapi-key": apiKey,
           "x-rapidapi-host": apiHost
