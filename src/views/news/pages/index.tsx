115df08d674cc212c4fa85fcdd71f4b1a0523f90
boooiler <sebastianowczarek8@gmail.com>
2024-03-02 23:20:24 +0100

    add news slider

--- src/views/news/pages/index.tsx
+++ src/views/news/pages/index.tsx
@@ -1,29 +1,69 @@
-import { useNews, useTopNews } from '../api/newsQuery'
-import './styles.scss'
+import { useEffect, useState } from 'react'
+import { useNews } from '../api/newsQuery'
 import Loader from '@/shared/components/loader'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import './styles.scss'
 
 export const News = () => {
-//   const { data: topNews, isLoading } = useTopNews("pl", "sports")
   const { data: topNews, isLoading } = useNews('sport', 'interia.pl, sport.interia.pl')
+  const [sliderNews, setSliderNews] = useState<any>()
+  const [otherNews, setOtherNews] = useState<any>()
+
+  useEffect(() => {
+    if(topNews && topNews.articles.length > 0) {
+      const { articles } = topNews
+      setSliderNews(articles.slice(0,4))
+      setOtherNews(articles.slice(4))
+      // setSliderNews(articles.slice(8,12))
+      // setOtherNews(articles.slice(12))
+    }
+  }, [topNews])
+
+  useEffect(() => {
+    console.log(sliderNews)
+  }, [sliderNews])
 
   return (
-    <section className="page-wrapper">
-      <h1 style={{ position: "sticky", top: "0" }}>Aktualności</h1>
+    <section className="page-wrapper articles-wrapper">
       {isLoading ? (
-        <Loader />
+        <Loader fullscreen />
       ) : !topNews || !topNews.articles.length ? (
         <>Błąd połączenia</>
       ) : (
-        <section className="articles">
-          {topNews.articles.map((news: any) => (
-            <a className='article-box' href={news.url} target='_blank' rel="noreferrer">
-              <div className="img-wrapper">
-                <img src={news.urlToImage} alt='image' />
-              </div>
-              <span>{news.title}</span>
-            </a>
-          ))}
-        </section>
+        <>
+          {sliderNews && (
+            <Carousel
+              showArrows={false}
+              autoPlay={false}
+              emulateTouch
+              infiniteLoop
+              interval={6000}
+              showStatus={false}
+              showThumbs={false}
+            >
+              {sliderNews.map((news: any, index: number) => (
+                <div className='article-box' key={index}>
+                  <div className="img-wrapper">
+                    <img src={news.urlToImage} alt='image' />
+                  </div>
+                  <span className='slide-text'>{news.title}</span>
+                </div>
+              ))}
+            </Carousel>
+          )}
+          <section className="articles">
+            <h1>Aktualności ze świata sportu</h1>
+            {otherNews && otherNews.map((news: any) => (
+              <a className='article-box' href={news.url} target='_blank' rel="noreferrer">
+                <div className="img-wrapper">
+                  <img src={news.urlToImage} alt='image' />
+                </div>
+                <span>{news.title}</span>
+              </a>
+            ))}
+          </section>
+        </>
       )}
     </section>
   )
